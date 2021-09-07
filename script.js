a20db5f8e3ac6b2532f032b3a2ef50056b17862a
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-09-07 20:29:23 +0200

    added css styles

---
+++ script.js
@@ -0,0 +1,7 @@
+const password = document.querySelector('.password')
+const confirmPassword = document.querySelector('.confirm-password')
+const submitBtn = document.querySelector('.submit-btn')
+
+submitBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+})
