0ef88448c1fc31ffc1481ac28062ad984be7cafb
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-09-08 11:45:52 +0200

    added js logic

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-const password = document.querySelector('.password')
-const confirmPassword = document.querySelector('.confirm-password')
-const submitBtn = document.querySelector('.submit-btn')
+const email = $('#email')
+const password = $('#password')
+const confirmPassword = $('#confirm-password')
+const phone = $('#phone')
+const submitBtn = $('.submit-btn')
 
-submitBtn.addEventListener('click', (e)=>{
+const checkInputs = ()=>{
+    const emailValue = email[0].value
+    const passwordValue = password[0].value
+    const confirmPasswordValue = confirmPassword[0].value
+    const phoneValue = phone[0].value
+
+    if(emailValue === ''){
+        setError(email)
+    } else if(!isValidEmail(emailValue)){
+        setError(email)
+    }
+
+    if(passwordValue === ''){
+        setError(password)
+    } else if (!isValidPassword(passwordValue)){
+        setError(password)
+    }
+
+    if(confirmPasswordValue !== passwordValue){
+        setError(password)
+        setError(confirmPassword)
+    }
+
+    if(!isValidNumber(phoneValue)){
+        setError(phone)
+    }
+}
+
+
+
+
+const setError = (input) => {
+    const pair = input.addClass("error")
+}
+
+const isValidEmail = (email) => {
+    return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(email)
+}
+
+const isValidPassword = (password) => {
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.test(password)
+}
+
+const isValidNumber = (number) => {
+    return /^\d+$/.test(number);
+}
+
+submitBtn.click((e)=> {
     e.preventDefault()
+    checkInputs()
 })
