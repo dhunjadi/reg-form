22e0e23bd9f1abeb5c9ea978cd67dfa5cdfd1e8d
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-09-08 16:00:24 +0200

    last touches

--- script.js
+++ script.js
@@ -3,55 +3,92 @@ const password = $('#password')
 const confirmPassword = $('#confirm-password')
 const phone = $('#phone')
 const submitBtn = $('.submit-btn')
+const passwordEye = document.getElementById('eye1')
+const confirmPasswordEye = document.getElementById('eye2')
+const message = document.querySelector('h1')
 
-const checkInputs = ()=>{
+submitBtn.click((e) => {
+    e.preventDefault()
+    checkInputs()
+})
+
+//Provjera inputa
+const checkInputs = () => {
     const emailValue = email[0].value
     const passwordValue = password[0].value
     const confirmPasswordValue = confirmPassword[0].value
-    const phoneValue = phone[0].value
 
-    if(emailValue === ''){
+    if (emailValue === '' || !isValidEmail(emailValue)) {
         setError(email)
-    } else if(!isValidEmail(emailValue)){
-        setError(email)
+    } else {
+        removeError(email)
     }
 
-    if(passwordValue === ''){
+    if (passwordValue === '' || !isValidPassword(passwordValue)) {
         setError(password)
-    } else if (!isValidPassword(passwordValue)){
-        setError(password)
+    } else {
+        removeError(password)
     }
 
-    if(confirmPasswordValue !== passwordValue){
+    if (confirmPasswordValue === '' || confirmPasswordValue !== passwordValue) {
         setError(password)
         setError(confirmPassword)
+    } else {
+        removeError(password)
+        removeError(confirmPassword)
     }
 
-    if(!isValidNumber(phoneValue)){
-        setError(phone)
+    if (isValidEmail(emailValue) && isValidPassword(passwordValue) && isValidPassword(confirmPasswordValue) && confirmPasswordValue === passwordValue) {
+        message.textContent = 'Registration Successful!'
+        message.style.color = 'green'
+        message.style.visibility = 'visible'
+    } else {
+        message.textContent = 'Something went wrong!'
+        message.style.color = 'red'
+        message.style.visibility = 'visible'
     }
+
+
 }
 
+// Prikazivanje/sakrivanje passworda
+passwordEye.addEventListener('click', () => {
+    if (password[0].attributes[0].nodeValue === 'password') {
+        password[0].attributes[0].nodeValue = 'text'
+        passwordEye.style.color = '#9db9f2'
+    } else {
+        password[0].attributes[0].nodeValue = 'password'
+        passwordEye.style.color = '#7e8fb3'
+    }
+})
 
+// Prikazivanje/sakrivanje confirmPassworda
+confirmPasswordEye.addEventListener('click', () => {
+    if (confirmPassword[0].attributes[0].nodeValue === 'password') {
+        confirmPassword[0].attributes[0].nodeValue = 'text'
+        confirmPasswordEye.style.color = '#9db9f2'
+    } else {
+        confirmPassword[0].attributes[0].nodeValue = 'password'
+        confirmPasswordEye.style.color = '#7e8fb3'
+    }
+})
 
-
+// Dodaje class "error"
 const setError = (input) => {
-    const pair = input.addClass("error")
+    input.addClass("error")
 }
 
+// Uklanja class "error"
+const removeError = (input) => {
+    input.removeClass("error")
+}
+
+// Provjera je li uneseni email validan
 const isValidEmail = (email) => {
     return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(email)
 }
 
+// Provjera je li uneseni password validan
 const isValidPassword = (password) => {
     return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.test(password)
 }
-
-const isValidNumber = (number) => {
-    return /^\d+$/.test(number);
-}
-
-submitBtn.click((e)=> {
-    e.preventDefault()
-    checkInputs()
-})
